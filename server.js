01d997f8196b12af9062b9312b0a3ae62dec4405
edirie <essedirie@gmail.com>
2022-03-06 14:51:35 -0500

    models and routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const session = require('express-session');
 const expbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
